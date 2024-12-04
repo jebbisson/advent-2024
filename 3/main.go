e46a165372f05e3aff0853b70b61da9458a7457a
JebBisson <jeb.bisson@gmail.com>
2024-12-03 19:58:58 -0500

    Solved 3 B

--- 3/main.go
+++ 3/main.go
@@ -24,42 +24,69 @@ func main() {
 
 	total := 0
 
-	r := regexp.MustCompile(`mul\(\d+,\d+\)`)
+	completeText := ""
+
 	for scanner.Scan() {
 		text := scanner.Text()
-		log.Printf("T: %v", text)
+		completeText += text
+	}
 
-		matches := r.FindAllString(text, -1)
-		if matches == nil {
-			log.Print("no matches found")
-		}
+	//split on dos
+	dos := strings.Split(completeText, "do()")
 
-		for _, val := range matches {
-			val = strings.ReplaceAll(val, "mul(", "")
-			val = strings.ReplaceAll(val, ")", "")
+	//reset so we can only regex valid do statements
+	completeText = ""
 
-			numbers := strings.Split(val, ",")
-
-			if len(numbers) < 2 {
-				log.Fatal("Less than 2 numbers found")
-			}
-
-			leftNum, err := strconv.Atoi(numbers[0])
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			rightNum, err := strconv.Atoi(numbers[1])
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			log.Printf("L: %v R: %v", leftNum, rightNum)
-
-			total += leftNum * rightNum
-
+	for _, val := range dos {
+		//find any don't in each do entry
+		donts := strings.Split(val, "don't()")
+		//since there are no do's in each val, dont means ignore all remaining.
+		if len(donts) > 0 {
+			//add only before the donts
+			completeText += donts[0]
+		} else {
+			//if not donts are found then block is good
+			completeText += val
 		}
 	}
 
+	log.Printf("T: %v", completeText)
+
+	r := regexp.MustCompile(`mul\(\d+,\d+\)`)
+
+	matches := r.FindAllString(completeText, -1)
+	if matches == nil {
+		log.Print("no matches found")
+	}
+
+	for _, val := range matches {
+		val = strings.ReplaceAll(val, "mul(", "")
+		val = strings.ReplaceAll(val, ")", "")
+
+		numbers := strings.Split(val, ",")
+
+		if len(numbers) < 2 {
+			log.Fatal("Less than 2 numbers found")
+		}
+
+		leftNum, err := strconv.Atoi(numbers[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		rightNum, err := strconv.Atoi(numbers[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("L: %v R: %v", leftNum, rightNum)
+
+		total += leftNum * rightNum
+
+	}
+
 	log.Printf("Total: %v", total)
+
+	//184576302
+	//302749809
 }
